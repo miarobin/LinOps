dd118ac1eb70ca83544c476e4cfc74bd48fce194
Mia West <mia.west@durham.ac.uk>
2024-02-07 11:56:07 +0000

    Lambda function fix

--- CrossSections.py
+++ CrossSections.py
@@ -1,5 +1,4 @@
 import lhapdf
-import scipy
 import numpy as np
 from scipy import integrate
 
@@ -8,8 +7,8 @@ s = 1000
 M = 800
 g = 1
 scale = s
-Ga = Gb = 1
 
+Ga = Gb = 1
 DCASIMIR = 1
 SUMOVERREPS = 1
 
@@ -26,7 +25,7 @@ f_G = lambda x: p.xfxQ2(21, x, s)
 #Gluon fusion matrix element over s (i.e. eq 70 in draft)
 MatrixElement_s = - Ga * Gb * SUMOVERREPS
 #Partonic cross section
-sigma_hat = - (MatrixElement_s / s) * 1/(12 * np.pi) * (1 + 2 * M**2 / s) * np.sqrt(1 - 4*M**2 / s) * DCASIMIR * g**4 / 4
+sigma_hat = lambda s: - (MatrixElement_s / s) * 1/(12 * np.pi) * (1 + 2 * M**2 / s) * np.sqrt(1 - 4*M**2 / s) * DCASIMIR * g**4 / 4
 
 
 #Computing the convolution of the gluon PDFs with the partonic cross section (i.e. eq 71 & 72 in draft)
