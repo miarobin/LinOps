3e64bcbec635737c307a7f308e022c49d991e05e
Mia West <mia.west@durham.ac.uk>
2024-02-07 18:17:04 +0000

    Alpha S

--- CrossSections.py
+++ CrossSections.py
@@ -28,8 +28,8 @@ MatrixElement_s = - Ga * Gb * SUMOVERREPS
 sigma_hat = lambda s: - (MatrixElement_s / s) * 1/(12 * np.pi) * (1 + 2 * M**2 / s) * np.sqrt(1 - 4*M**2 / s) * DCASIMIR * g**4 / 4
 
 
-#Computing the convolution of the gluon PDFs with the partonic cross section (i.e. eq 71 & 72 in draft)
-partonic = lambda x, y: (f_G(x) * f_G(y) * sigma_hat(x*y*s) / (x*y)) * np.pi * p.ALPHAS(s)**2 * DCASIMIR / (8 * DCASIMIR**2 * 12) 
+#Computing the convolution of the gluon PDFs with the partonic cross section (i.e. eq 71 & 72 in draft)(NOT FINISHED)
+partonic = lambda x, y: (f_G(x) * f_G(y) * sigma_hat(x*y*s) / (x*y)) * np.pi * p.AlphaS(s)**2 * DCASIMIR / (8 * DCASIMIR**2 * 12) 
 hadronic, err = integrate.nquad(partonic, [[0, 1],[0, 1]])
 
 print(hadronic)
