c76e2dfe250c8280e54919c46f5cf62ec74951ae
Mia West <mia.west@durham.ac.uk>
2024-02-07 11:01:39 +0000

    Fixed expression

--- CrossSections.py
+++ CrossSections.py
@@ -7,7 +7,7 @@ s = 1000
 M = 800
 g = 1
 scale = s
-
+Ga = Gb = 1
 
 DCASIMIR = 1
 SUMOVERREPS = 1
@@ -25,7 +25,7 @@ f_G = lambda x: p.xfxQ2(21, x, s)
 #Gluon fusion matrix element over s (i.e. eq 70 in draft)
 MatrixElement_s = - Ga * Gb * SUMOVERREPS
 #Partonic cross section
-sigma_hat = - (MatrixElement_s / s) * 1/(12 * np.pi) * (1 + 2 M**2 / s) * np.sqrt(1 - 4*M**2 / s) * DCASIMIR * g**4 / 4
+sigma_hat = - (MatrixElement_s / s) * 1/(12 * np.pi) * (1 + 2 * M**2 / s) * np.sqrt(1 - 4*M**2 / s) * DCASIMIR * g**4 / 4
 
 
 #Computing the convolution of the gluon PDFs with the partonic cross section (i.e. eq 71 & 72 in draft)
