1ef1a614dd056d4ab1f7b22e161a683550af433f
Mia West <mia.west@durham.ac.uk>
2024-02-08 10:31:01 +0000

    Varying s_max and plotting

--- CrossSections.py
+++ CrossSections.py
@@ -1,6 +1,16 @@
 import lhapdf
 import numpy as np
 from scipy import integrate
+from matplotlib import pyplot as plt
+
+'''
+TO DO:
+- Sort out alpha_s.
+- Constants e.g. M, g, ...
+- Add in other processes (e.g. non-fermions, different production mechanisms, ...).
+- Check sigma_hat (i.e. manually set range of validity).
+- Check against a known process.
+'''
 
 #Pre-calculation variables:
 s_max = (1e4)**2 #GeV
@@ -23,7 +33,7 @@ alpha_S = lhapdf.mkAlphaS("CT10nlo")
 
 
 #Need to test the PDF set does as expected.
-f_G = lambda x: p.xfxQ2(21, x, s_max)
+f_G = lambda x, s_max: p.xfxQ2(21, x, s_max)
 
 ##Partonic cross section. 
 #Gluon fusion matrix element over s (i.e. eq 70 in draft)
@@ -32,9 +42,15 @@ MatrixElement_s = - Ga * Gb * SUMOVERREPS
 sigma_hat = lambda s: - (MatrixElement_s / s) * 1/(12 * np.pi) * (1 + 2 * M**2 / s) * np.sqrt(1 - 4*M**2 / s) * DCASIMIR * g**4 / 4 if s>=4*M**2 else 0
 
 
-#Computing the convolution of the gluon PDFs with the partonic cross section (i.e. eq 71 & 72 in draft)(NOT FINISHED)
-partonic = lambda x, y: (f_G(x) * f_G(y) * sigma_hat(x*y*s_max) / (x*y)) * np.pi * alpha_S.alphasQ(x*y*s_max)**2 * DCASIMIR / (8 * DCASIMIR**2 * 12) 
-hadronic, err = integrate.nquad(partonic, [[0, 1],[0, 1]])
-
-print(hadronic)
-print(err)
+#Run over s_max:
+s_maxs = np.power(np.arange(100,1400),2)
+hadronics = []
+for s_max in s_maxs:
+    #Computing the convolution of the gluon PDFs with the partonic cross section (i.e. eq 71 & 72 in draft)(NOT FINISHED)
+    partonic = lambda x, y: (f_G(x) * f_G(y) * sigma_hat(x*y*s_max) / (x*y)) * np.pi * alpha_S.alphasQ(x*y*s_max)**2 * DCASIMIR / (8 * DCASIMIR**2 * 12) 
+    hadronic, err = integrate.nquad(partonic, [[0, 1],[0, 1]])
+    hadronic.append(hadronics)
+    
+plt.plot(hadronics, s_maxs)
+plt.show()
+    
