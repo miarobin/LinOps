ddc040079a4b479836b8d17a5b7383b0abe328ba
Mia West <mia.west@durham.ac.uk>
2024-02-23 10:35:39 +0000

    .

--- CrossSections.py
+++ CrossSections.py
@@ -77,7 +77,7 @@ LHC = (13.5e3)**2 #GeV^2
 
 results = []; betasqs = np.linspace(0,1)
 for betasq in betasqs:
-    results.append([G_scalar(betasq),G_fermion(betasq)])
+    results.append([G_fermion(betasq),G_scalar(betasq)])
 results = np.array(results)
 plt.plot(betasqs,results[:,0],color='red')
 plt.plot(betasqs,results[:,1],color='blue')
