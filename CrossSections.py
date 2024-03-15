bf402cdae2c2d7c570d68fa5c48c1f7f2542d1da
Mia West <mia.west@durham.ac.uk>
2024-03-15 10:03:33 +0000

    Test

--- CrossSections.py
+++ CrossSections.py
@@ -227,6 +227,7 @@ for Mn in MLQs:
 
     #GLUONS.
     r = lambda n, m: 3 * dc(n,m) / Dc(n,m) * (3**2-1)
+    print(r(*Zs['Delta'][0:2]))
     sigma_GGs = integrate.nquad(lambda x,y: f_G(x,x*y*TEV)*f_G(y,x*y*TEV)*G_scalar(betasq(x,y),r(*Zs['Delta'][0:2]))/(x*y)**2,[[0.001,1],[0.001,1]])[0]
 
     #Multiply by constants & add to result array.
