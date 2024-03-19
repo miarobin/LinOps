fdd95bbe4767f10cf6e380ca29cce4e3d47b0fcd
Mia West <mia.west@durham.ac.uk>
2024-03-19 12:45:54 +0000

    New test for new heavy leptons

--- CrossSections.py
+++ CrossSections.py
@@ -3,6 +3,10 @@ import numpy as np
 from scipy import integrate
 from matplotlib import pyplot as plt
 
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.fontset"] = "cm"
+plt.rcParams["font.size"]= 12
+
 
 #Import the PDF sets. Decide on the most appropriate one later.
 p = lhapdf.mkPDF("PDF4LHC15_nlo_100", 0)
@@ -25,7 +29,7 @@ Zs = {'Xi':[0, 0, 1/2],
 #Gluon PDF
 f_G = lambda x, s_max: p.xfxQ2(21, x, s_max)
 #Strong coupling constant running.
-alpha_S = lhapdf.mkAlphaS("CT18NNLO")
+alpha_S = lhapdf.mkAlphaS("PDF4LHC15_nlo_100")
 mW = 80; mZ = 91; v = 246
 alpha_w = ((2*mZ/v)**2 - (2*mW/v)**2)/(4*np.pi)
 alpha_Y = (2*mW/v)**2/(4*np.pi)
@@ -69,7 +73,7 @@ def F_fermion(betasq):
 
 
 #HADRONIC CROSS-SECTION
-Mnews=np.linspace(400,1400,num=10)
+Mnews=np.linspace(400,1400,num=3)
 LHC = (13.5e3)**2 #GeV^2
 
 print(f"Alpha_s at Z pole: {alpha_S.alphasQ2(90**2)}")
@@ -122,7 +126,7 @@ for Mn in Mnews:
     NPf.append(fresults_)
     NPs.append(sresults_)
 
-NPf = np.array(NPf); NPs = np.array(NPs)
+NPf = np.array(NPf)/(2.56819e-9); NPs = np.array(NPs)/(2.56819e-9)
 plt.plot(Mnews,NPf[:,0],color='red'); plt.plot(Mnews,NPs[:,0],color='red',linestyle='dashed')
 plt.plot(Mnews,NPf[:,1],color='blue'); plt.plot(Mnews,NPs[:,1],color='blue',linestyle='dashed')
 plt.plot(Mnews,NPf[:,2],color='black'); plt.plot(Mnews,NPs[:,2],color='black',linestyle='dashed')
@@ -131,8 +135,8 @@ plt.plot(Mnews,NPf[:,4],color='orange'); plt.plot(Mnews,NPs[:,4],color='orange',
 plt.plot(Mnews,NPf[:,5],color='green'); plt.plot(Mnews,NPs[:,5],color='green',linestyle='dashed')
 plt.xscale('log')
 plt.yscale('log')
-plt.xlabel("Mass of New Particle")
-plt.ylabel("Cross Section")
+plt.xlabel("Mass of New Particle (GeV)")
+plt.ylabel("Cross Section (pb)")
 plt.savefig("testing.pdf", format="pdf", bbox_inches="tight")
 
 plt.figure()
@@ -240,3 +244,42 @@ plt.savefig("LeptoquarkTest.pdf", format="pdf", bbox_inches="tight")
 
 plt.figure()
 '''
+
+#Testing against heavy leptons at LHC
+MLQs=np.linspace(500,2000,num=5)
+TEV = (14e3)**2 #GeV^2
+
+results = []
+for Mn in MLQs:
+    betasq = lambda x,y: 1 - 4*Mn**2/(TEV*x*y)
+    
+    dL = lambda n: n+1 ; DL = lambda n: n*(n+1)*(n+2)/(3*2*2)
+    dc = lambda n,m: (m+1)*(n+1)*(n+m+2)/2 ; Dc = lambda n,m :(m**3 + n**3 + 3*(n+m) + m*n) * dc(n,m)/ (4*3*2)
+    
+    constsGG = lambda nL, nC: np.pi * dL(nL) * 4 * Dc(nC,0)**2 / (TEV*dc(nC,0))
+    constsqqL = lambda nL, nC: np.pi*alpha_w**2 * dc(nC,0) * dL(nL) / TEV
+    constsqqY = lambda nL, nC, QY: np.pi * alpha_Y**2 * QY**2 * dc(nC,0) * dL(nL) / TEV
+
+    #NOTE the PDFs from LHAPDF are of the form f_LHAPDF = xf_DRAFT(x).
+
+    sigma_qqYf = integrate.nquad(lambda x,y: F_fermion(betasq(x,y))/(x*y)**2 *\
+                            np.sum([(QUARKS[q][2](x,x*y*LHC)*ANTIQUARKS[q][2](y,x*y*LHC))*(QUARKS[q][0]**2+QUARKS[q][1]**2) for q in ['u','d']]),[[0.001,1],[0.001,1]])[0]
+    
+    sigma_qqLf = integrate.nquad(lambda x,y: F_fermion(betasq(x,y))/(x*y)**2 *\
+                            (2*(QUARKS['u'][2](x,x*y*LHC)*ANTIQUARKS['d'][2](y,x*y*LHC) + QUARKS['d'][2](x,x*y*LHC)*ANTIQUARKS['u'][2](y,x*y*LHC)) +\
+                            (QUARKS['u'][2](x,x*y*LHC)*ANTIQUARKS['u'][2](y,x*y*LHC) + QUARKS['d'][2](x,x*y*LHC)*ANTIQUARKS['d'][2](y,x*y*LHC))),[[0.001,1],[0.001,1]])[0]/4
+
+    #Multiply by constants & add to result array.
+    
+    QQLL = constsqqL(1,0)*sigma_qqLf + constsqqY(1,0,-1/2)*sigma_qqLf
+    
+    results.append(3*QQLL)
+
+results = np.array(results)
+plt.figure()
+
+plt.plot(MLQs,results/(2.56819e-9),color='red')
+plt.yscale('log')
+plt.xlabel("Mass of New Particle")
+plt.ylabel("Cross Section")
+plt.savefig("LeptoquarkTest.pdf", format="pdf", bbox_inches="tight")
